bff38915250985868f8b78e956e5ad2a97d12617
Chongzhi Yin <yinchongzhi@gmail.com>
2023-10-18 22:32:33 +0800

    [fix] bugs

--- scale-rm/test/case/shallowcloud/SDM128_50x50x5/run.sh
+++ scale-rm/test/case/shallowcloud/SDM128_50x50x5/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-JID=`pjsub -z jid --step --sparam "sn=1" -N $(basename $PWD) SDM_job.sh
+JID=`pjsub -z jid --step --sparam "sn=1" -N $(basename $PWD) SDM_job.sh`
 cd ./results
 pjsub --step --sparam "jid=${JID%??}, sn=2, sd=ec!=0:after:1" ncl.sh
 pjsub --step --sparam "jid=${JID%??}, sn=3, sd=ec!=0:after:2" merge.sh
