689a2ce3dd9dcc9339c972ada6e7f57373d23a5f
Chongzhi Yin <yinchongzhi@gmail.com>
2023-10-18 21:10:06 +0800

    [add] setups for the rerun

---
+++ scale-rm/test/case/shallowcloud/SDM64_12.5x12.5x10/results/ncl.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#------ pjsub option --------#
+#PJM -L rscgrp=sa
+#PJM -L node=2
+#PJM --mpi proc=80
+#PJM -L elapse=24:00:00
+#PJM -g r22775
+#PJM -j
+#------- Program execution -------#
+module load intel impi hdf5 netcdf netcdf-fortran
+ulimit -u 2048
+export OPENBLAS_NUM_THREADS=1
+source ~/.bashrc
+conda activate ncl_stable
+# run
+rm -rf SDM_history_*.ncl
+MPI=7680
+core_num=80
+let p=MPI/core_num
+i=0
+while(( $i<$(($core_num-1)) ))
+do
+    cp ./SDM_history.ncl ./SDM_history_$(($i*$p)).ncl
+    sed -i "s/0,MPI/$(($i*$p)),$((($i+1)*$p))/" ./SDM_history_$(($i*$p)).ncl
+    ncl SDM_history_$(($i*$p)).ncl &
+    let "i++"
+done
+cp ./SDM_history.ncl ./SDM_history_$(($i*$p)).ncl
+sed -i "s/0,MPI/$(($i*$p)),$((($i+1)*$p))/" ./SDM_history_$(($i*$p)).ncl
+ncl SDM_history_$(($i*$p)).ncl
+wait
---
+++ scale-rm/test/case/shallowcloud/SDM64_50x50x2.5/SDM_job.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#------ pjsub option --------#
+#PJM -L rscgrp=ha
+#PJM -L node=40
+#PJM --mpi proc=1600
+#PJM -L elapse=120:00:00
+#PJM -g r22775
+#PJM -j
+#------- Program execution -------#
+module load intel impi hdf5 netcdf netcdf-fortran
+source ~/.bashrc
+# run
+mpiexec.hydra -n ${PJM_MPI_PROC} ./scale-rm_init init.conf || exit
+mpiexec.hydra -n ${PJM_MPI_PROC} ./scale-rm run.conf || exit
